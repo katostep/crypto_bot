0716e1c386b60cc0c0afebe4138524e9d717cd53
katostep <53716051+katostep@users.noreply.github.com>
2022-01-27 23:27:21 +0900

    Add first files

---
+++ crypto_secret.py
@@ -0,0 +1,28 @@
+import getpass
+import cryptocode
+
+# ターミナルで入力した文字をパスワードで暗号化してファイル保存する
+def encrypto_save(file_path):
+	if not file_path:
+		print("Please filename!!!")
+		return
+	data = getpass.getpass("api secret:")
+	password = getpass.getpass('password:')
+	
+	cipher = cryptocode.encrypt(data, password)
+
+	with open(file_path, 'w') as f:
+		f.write(cipher)
+
+# 複号化する
+def decrypto(file_path):
+	password = getpass.getpass('password:')
+	f = open(file_path, 'r' , encoding='UTF-8')
+	decoded = cryptocode.decrypt(f.read(),password)
+
+	return decoded
+
+if __name__ == "__main__":
+	file_path = 'aaa'	
+	#encrypto_save(file_path)
+	print(decrypto(file_path))
